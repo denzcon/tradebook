1ccc8979e9d585a200af17166620a4c120760102
Jonathan Conoley <jonathanc@Jonathans-New-Mac-mini.local>
2012-09-04 19:20:24 -0700

    forgot to add this file

---
+++ trunk/www/application/views/modals/manage_xp.php
@@ -0,0 +1,12 @@
+<?php ?>
+    <div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h4>Manage XP <small>on connected accounts</small></h4>
+    </div>
+    <div class="modal-body">
+		<p>One fine body…</p>
+    </div>
+    <div class="modal-footer">
+		<a href="#" class="btn">Close</a>
+		<a href="#" class="btn btn-primary">Save changes</a>
+    </div>
