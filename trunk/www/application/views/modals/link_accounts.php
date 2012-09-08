95d58d740e59b4369dbb44abba06c4a072139452
Jonathan Conoley <jonathanc@Jonathans-New-Mac-mini.local>
2012-09-08 00:13:21 -0700

    forgot a file

---
+++ trunk/www/application/views/modals/link_accounts.php
@@ -0,0 +1,36 @@
+<!-- linkAccounts Modal-->
+<div class="linkAccounts modal">
+	<div class="linkAccounts alert alert-info">
+		<h1>Link Accounts <small>to receive XP</small></h1>
+		<ul>
+			<li>
+				<div class="well linkAccounts">
+						<label>User's First/Last Name</label>
+						<div class="input-append">
+							<input type="text" placeholder="Type something…" class="linkAccounts span3" name="email">
+						</div>
+				</div>
+			</li>
+			<li class="well linkedData">
+				<table class="table">
+					<thead>
+						<tr class="tbl-header">
+							<th>Username</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Statusl</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Row 1</td>
+							<td>Row 1</td>
+							<td>Row 1</td>
+							<td>Row 1</td>
+						</tr>
+					</tbody>        
+				</table>
+			</li>
+		</ul>
+	</div>
+</div>
